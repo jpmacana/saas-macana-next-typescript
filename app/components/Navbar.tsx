c0ad981014259c63ec7151ed9fdc92b6ac841dec
Juan Pablo Senatra <jpsenatra@gmail.com>
2024-03-11 17:23:19 -0300

    add proyect

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+import { ThemeToggle } from "@/app/components/themetoggle";
+
+
+export async function Navbar() {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-3xl">
+            Marshal<span className="text-primary">Saas</span>
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-x-5">
+          <ThemeToggle />
+
+          {(await isAuthenticated()) ? (
+          
+          <LogoutLink>
+
+          <Button>Sign Out</Button>
+          </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+
+              <RegisterLink>
+                <Button variant="secondary">Sign Up</Button>
+              </RegisterLink>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+
+import { ThemeProvider } from "@/app/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+import { Navbar } from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar/>
+        {children}
+        </ThemeProvider></body>
     </html>
   );
 }
