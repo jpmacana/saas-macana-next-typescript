2b69f877c86633a1aef459f93a089ad63ed049a6
Juan Pablo Senatra <jpsenatra@gmail.com>
2024-03-18 16:24:51 -0300

    add descripciones2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Navbar } from "./components/Navbar";
 
 import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export const metadata: Metadata = {
 };
 
 async function getData(userId: string) {
+  noStore();
   if (userId) {
     const data = await prisma.user.findUnique({
       where: {
