dadec173fe0d6ed5dbe6bc840845b5a11421fd6e
Juan Pablo Senatra <jpsenatra@gmail.com>
2024-03-18 16:08:28 -0300

    add new and edit note

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/db";
 import { CheckCircle2 } from "lucide-react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { getStripeSession } from "@/lib/stripe";
+import { getStripeSession,stripe } from "@/lib/stripe";
 import { redirect } from "next/navigation";
-import { StripeSubscriptionCreationButton } from "@/app/components/SubmitButtons";
+import { StripePortal, StripeSubscriptionCreationButton } from "@/app/components/SubmitButtons";
 
 const featureItems = [
   { name: "Lorem ipsum" },
@@ -15,6 +15,7 @@ const featureItems = [
   { name: "Lorem ipsum" },
   { name: "Lorem ipsum" },
 ];
+
 
 async function getData(userId: string) {
   const data = await prisma.subscription.findUnique({
@@ -32,6 +33,7 @@ async function getData(userId: string) {
   });
   return data;
 }
+
 
 export default async function BillingPage() {
   const { getUser } = getKindeServerSession();
@@ -59,7 +61,47 @@ export default async function BillingPage() {
       priceId: process.env.STRIPE_PRICE_ID as string,
     });
 
-    return redirect(subscriptionUrl);
+    return redirect(subscriptionUrl);}
+
+    async function createCustomerPortal() {
+      "use server";
+      const session = await stripe.billingPortal.sessions.create({
+        customer: data?.user.stripeCustomerId as string,
+        return_url:'http://localhost:3000/dashboard'
+      });
+         return redirect(session.url);
+    }
+
+if (data?.status === 'active') {
+  return (
+
+    <div className="grid items-start gap-8">
+    <div className="flex items-center justify-between px-2">
+      <div className="grid gap-1">
+        <h1 className="text-3xl md:text-4xl ">Subscription</h1>
+        <p className="text-lg text-muted-foreground">
+          Settings reagding your subscription
+        </p>
+      </div>
+    </div>
+    <Card className="w-full lg:w-2/3">
+          <CardHeader>
+            <CardTitle>Edit Subscription</CardTitle>
+            <CardDescription>
+              Click on the button below, this will give you the opportunity to
+              change your payment details and view your statement at the same
+              time.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form >
+              <StripePortal />
+            </form>
+          </CardContent>
+        </Card>
+    </div>
+);
+
   }
 
   return (
---
+++ app/dashboard/new/page.tsx
@@ -0,0 +1,83 @@
+import { SubmitButton } from "@/app/components/SubmitButtons";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/db";
+
+export default async function NewNoteRoute() {
+    const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+
+    async function postData(formData: FormData) {
+        "use server";
+    
+        if (!user) {
+          throw new Error("Not authorized");
+        }
+    
+        const title = formData.get("title") as string;
+        const description = formData.get("description") as string;
+    
+        await prisma.note.create({
+          data: {
+            userId: user?.id,
+            description: description,
+            title: title,
+          },
+        });
+    
+        return redirect("/dashboard");
+      }
+  return (
+    <Card>
+      <form action={postData}>
+        <CardHeader>
+          <CardTitle>New Note</CardTitle>
+          <CardDescription>
+            Aca es donde podes crear tus notas 😁
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-y-5">
+          <div className="gap-y-2 flex flex-col">
+            <Label>Titulo</Label>
+            <Input
+              required
+              type="text"
+              name="title"
+              placeholder="Titulo de la nota..."
+            />
+          </div>
+
+          <div className="flex flex-col gap-y-2">
+            <Label>Descripcion</Label>
+            <Textarea
+              name="description"
+              placeholder="Escribi lo que vos quieras..."
+              required
+            />
+          </div>
+        </CardContent>
+
+        <CardFooter className="flex justify-between">
+          <Button asChild variant="destructive">
+            <Link href="/dashboard">Cancelar</Link>
+          </Button>
+          <SubmitButton />
+        </CardFooter>
+      </form>
+    </Card>
+  );
+}
